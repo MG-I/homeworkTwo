75634b8184c0981611a8eaba31dade07ad5baa8d
GolubtsevaMI <maria.golubtseva.extern@elinvar.de>
2022-07-12 16:20:31 +0300

    HomeworkTwoNew

--- src/NameGenerationInterface.java
+++ src/NameGenerationInterface.java
@@ -1,7 +1,7 @@
 public interface NameGenerationInterface {
 
-    String[] namesCar = {"BMW", "Mercedes", "Volvo", "Lada", "UAZ", "Ford"};
-    String[] modelCar = {"X5", "GLA", "S60", "Vesta", "2101", "Escape"};
+    String[] namesCar = {"BMW", "Mercedes", "Volvo", "Lada", "UAZ", "Ford", "Volvo"};
+    String[] modelCar = {"X5", "GLA", "S60", "Vesta", "2101", "Escape", "S40"};
 
 
 }
